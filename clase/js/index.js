80cdabca9734aba672eeff2a5f84f42fe9a42062
SantiCinconegui <santiagocinconegui@gmail.com>
2021-12-23 21:31:54 -0300

    commit java

---
+++ clase/js/index.js
@@ -0,0 +1,6 @@
+//let nombreApellido = "santiago cinconegui"
+  
+let nombreApellido = prompt('ingrese su nombre')
+console.log(nombreApellido)
+alert('bienvenido! ' +  nombreApellido)
+
---
+++ js-coder/js/main.js
@@ -0,0 +1,6 @@
+let num = Number (prompt("Ingrese su edad, para comprobar que ud es mayor de edad"));
+if (num >= 18){
+    alert("BIENVENIDO")
+}else if (num < 18){
+    alert("NO PUEDES INGRESAR PORQUE NO CUMPLES CON LOS REQUISITOS")
+}
